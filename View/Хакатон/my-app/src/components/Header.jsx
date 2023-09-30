44aa4817755bfd53ee765ae81b684569a51d9c6c
Gambit201 <146526023+Gambit201@users.noreply.github.com>
2023-10-01 02:15:11 +0700

    create view

---
+++ View/Хакатон/my-app/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Header = () => {
+    return <header>
+        <Container>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">SOS</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">ГЛАВНАЯ БЛЯТЬ</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">О программе</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Справка</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </Container>
+    </header>
+}
+
+export default Header
