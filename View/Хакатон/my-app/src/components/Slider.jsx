e84d44980914aa6b9f5d9fa861f80c8e2eb07b8b
Gambit201 <146526023+Gambit201@users.noreply.github.com>
2023-10-01 04:13:08 +0700

    create form

--- View/Хакатон/my-app/src/components/Slider.jsx
+++ View/Хакатон/my-app/src/components/Slider.jsx
@@ -1,42 +1,42 @@
 import React from "react"
-import { Carousel, CarouselCaption, CarouselItem } from "react-bootstrap"
-import NeverGonna1 from "./Images/Never-Gonna1.jpg"
-import NeverGonna2 from "./Images/Never-Gonna2.jpg"
-import NeverGonna3 from "./Images/Never-Gonna3.jpg"
+import { Carousel } from "react-bootstrap"
+import Photo1 from "./Images/Photo1.jpeg"
+import Photo2 from "./Images/Photo2.jpeg"
+import Photo3 from "./Images/Photo3.jpeg"
 
 export default function Slider (){
     return (
-    <Carousel>
-        <Carousel.Item>
-            <img 
-                className="d-block w-100"
-                src={NeverGonna1}
-                alt="Never gonna give you up"
-            />
-            <Carousel.Caption>
-                <h3>Never gonna give you up</h3>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img 
-                className="d-block w-100"
-                src={NeverGonna2}
-                alt="Never gonna let you down"
-            />
-            <Carousel.Caption>
-                <h3>Never gonna let you down</h3>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img 
-                className="d-block w-100"
-                src={NeverGonna3}
-                alt="Never gonna run around and desert you"
-            />
-            <Carousel.Caption>
-                <h3>Never gonna run around and desert you</h3>
-            </Carousel.Caption>
-        </Carousel.Item>
-    </Carousel>
+        <Carousel>
+            <Carousel.Item>
+                <img 
+                    className="d-block w-100"
+                    src={Photo1}
+                     alt=". . ."
+                />
+                <Carousel.Caption>
+                    <h3>Енотики</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img 
+                    className="d-block w-100"
+                    src={Photo2}
+                    alt=". . . "
+                />
+                <Carousel.Caption>
+                    <h3>Кролик</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img 
+                    className="d-block w-100"
+                    src={Photo3}
+                    alt=". . . "
+                />
+                <Carousel.Caption>
+                    <h3>Красная Панда</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
 )}
 
