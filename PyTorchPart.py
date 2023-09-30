c98c2f423174712d3a84e36e30ac2da27db141f7
Kein <deryabin.2001@inbox.ru>
2023-09-30 21:20:27 +0700

    PyTorch data lock

--- PyTorchPart.py
+++ PyTorchPart.py
@@ -1,75 +1,88 @@
+import os
+import shutil
 import torch
 import torch.nn as nn
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-from torchvision.datasets import ImageFolder
+import torch.optim as optim
+from torchvision import transforms, datasets
+import torchvision.transforms.functional as F
 
-# Определение модели нейронной сети
-class ImageClassifier(nn.Module):
+# Предварительная обработка данных и загрузка
+transform = transforms.Compose([transforms.Resize((128, 128)),
+                                transforms.ToTensor()])
+
+train_data = datasets.ImageFolder("E:\Python\Phototraps_shots_classification\Classes", transform=transform)
+test_data = datasets.ImageFolder("E:\Python\Phototraps_shots_classification\Train", transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
+
+# Создание сверточной нейронной сети (CNN)
+class Net(nn.Module):
     def __init__(self):
-        super(ImageClassifier, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.classifier = nn.Linear(32 * 8 * 8, 2)  # 2 класса: качественные и некачественные фотографии
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 30 * 30, 128)
+        self.fc2 = nn.Linear(128, 2)  # Два класса: качественные и некачественные
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = x.view(-1, 32 * 30 * 30)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
-# Загрузка и предобработка данных
-transform = transforms.Compose([
-    transforms.Resize((32, 32)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+net = Net()
 
-train_dataset = ImageFolder('путь_к_папке_с_обучающими_изображениями', transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+# Определение функции потерь и оптимизатора
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 # Обучение модели
-model = ImageClassifier()
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-num_epochs = 10
-device = torch.device('cpu')
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
-
-for epoch in range(num_epochs):
-    for images, labels in train_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-
+for epoch in range(1000):  # Пример: 1000 эпох
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
         optimizer.zero_grad()
-        outputs = model(images)
+        outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
+        running_loss += loss.item()
+    print(f'Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}')
 
-    print(f'Эпоха [{epoch+1}/{num_epochs}], Потеря: {loss.item()}')
+print('Обучение завершено')
 
-# Теперь модель обучена. Можно использовать ее для классификации новых изображений.
-# Например, для классификации изображения 'image.jpg':
+# Создайте папки "Useful" и "Trash" если их нет
+if not os.path.exists('Useful'):
+    os.makedirs('Useful')
+if not os.path.exists('Trash'):
+    os.makedirs('Trash')
 
-test_image = Image.open('image.jpg')
-test_image = transform(test_image).unsqueeze(0)
-test_image = test_image.to(device)
+# Функция для переноса изображений в соответствующие папки
+def move_images(predictions):
+    for i, prediction in enumerate(predictions):
+        image_path = test_data.samples[i][0]  # Путь к изображению
+        if prediction == 0:  # Предсказание: качественное
+            shutil.copy(image_path, 'Useful')
+        else:  # Предсказание: некачественное
+            shutil.copy(image_path, 'Trash')
 
-model.eval()
+# Оценка модели на тестовом наборе данных
+correct = 0
+total = 0
+predictions = []
+
 with torch.no_grad():
-    outputs = model(test_image)
-    _, predicted = torch.max(outputs.data, 1)
+    for data in test_loader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        predictions.extend(predicted.tolist())
 
-if predicted.item() == 0:
-    print('Изображение является качественным.')
-else:
-    print('Изображение является некачественным.')
+print(f'Точность на тестовом наборе данных: {100 * correct / total}%')
+
+# Перемещение изображений в соответствующие папки
+move_images(predictions)
