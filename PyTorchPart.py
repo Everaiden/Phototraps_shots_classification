7e42b842bd62f8c43a9ac8055977fa7c5f781ce3
Kein <deryabin.2001@inbox.ru>
2023-10-01 09:27:25 +0700

    Check on good/bad photos with dearhivization and save learning

--- PyTorchPart.py
+++ PyTorchPart.py
@@ -8,13 +8,27 @@ from torch.utils.data import DataLoader
 import torchvision.transforms.functional as F
 from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+import zipfile
+
+## Замените 'имя_архива.zip' на фактическое имя вашего архива.
+#архив = zipfile.ZipFile('имя_архива.zip', 'r')
+
+## Указываете папку, в которую хотите разархивировать содержимое.
+#папка_разархивации = 'путь_к_папке_разархивации'
+
+## Создайте папку, если она не существует
+#if not os.path.exists(папка_разархивации):
+#    os.makedirs(папка_разархивации)
+
+## Извлекаем все файлы из архива
+#архив.extractall(папка_разархивации)
+
+## Закрываем архив
+#архив.close()
 
 # Предварительная обработка данных и загрузка
 transform = transforms.Compose([transforms.Resize((128, 128)),
                                 transforms.ToTensor()])
-
-#train_data = datasets.ImageFolder(".\Classes", transform=transform)
-#test_data = datasets.ImageFolder(".\Train", transform=transform)
 
 train_data = datasets.ImageFolder('.\Classes', transform=transform)
 test_data = DataLoader(train_data, batch_size=32, shuffle=True)
@@ -34,10 +48,10 @@ class Net(nn.Module):
         # Определение линейных слоев
         self.fc2 = nn.Linear(128, 2)
         if self.fc2 == 3969:
-            self.fc1 = nn.Linear(3969, 128)  # 32 channels, 30x30 output size after conv and pool
+            self.fc1 = nn.Linear(3969, 128)
         else:
-            self.fc1 = nn.Linear(127008, 128)  # 32 channels, 30x30 output size after conv and pool
-        #self.fc2 = nn.Linear(128, 2)
+            self.fc1 = nn.Linear(127008, 128)
+
 
     def forward(self, x):
         x = self.pool(torch.nn.functional.relu(self.conv1(x)))
@@ -53,7 +67,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 # Обучение модели
-for epoch in range(10):  # Пример: 1000 эпох
+for epoch in range(1):
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
         inputs, labels = data
@@ -66,6 +80,14 @@ for epoch in range(10):  # Пример: 1000 эпох
     print(f'Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}')
 
 print('Обучение завершено')
+
+# Сохранение модели
+torch.save(net.state_dict(), 'Model.pth')
+print('Модель сохранена в файл модель.pth')
+# Загрузка модели
+#loaded_net = Net()
+#loaded_net.load_state_dict(torch.load('модель.pth'))
+#loaded_net.eval()  # Убедитесь, что модель находится в режиме оценки, если вы не планируете продолжать обучение
 
 # Создайте папки "Useful" и "Trash" если их нет
 if not os.path.exists('Useful'):
